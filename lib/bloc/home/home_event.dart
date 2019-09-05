52e14db7ac97369f06478a40d685c24182b66b75
nha nguyen <nguyenhoangvannha@gmail.com>
2019-09-05 11:12:01 +0700

    remove: home bloc

--- lib/bloc/home/home_event.dart
+++
@@ -1,7 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
-@immutable
-abstract class HomeEvent extends Equatable {
-  HomeEvent([List props = const []]) : super(props);
-}
--- lib/bloc/home/home_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
-@immutable
-abstract class HomeState extends Equatable {
-  HomeState([List props = const []]) : super(props);
-}
-
-class InitialHomeState extends HomeState {}
