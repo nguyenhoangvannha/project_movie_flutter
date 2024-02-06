e376d19dbf656c5ae677c650aa0b91c23e597b5a
NHV Nha <nguyenhoangvannha@gmail.com>
2024-02-06 11:44:12 +0700

    dart fix apply

--- lib/component/api_config.dart
+++ lib/component/api_config.dart
@@ -1,5 +1,5 @@
 abstract class ApiConfig {
-  static const String API_KEY = 'afc072076fd42b05abd1431a0f45c601';
+  static const String API_KEY = '';
   static const String BASE_API_URL = 'https://api.themoviedb.org/3';
   static const String BASE_POSTER_URL = 'https://image.tmdb.org/t/p/w342';
 }
